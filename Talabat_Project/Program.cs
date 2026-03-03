a9f5b58e4dd5feb4204ae6f3467b5f92c26ad5eb
Abdallah Elhabash <abdallahm.elhabash@gmail.com>
2026-03-03 05:32:59 +0200

    Beginig of Project

---
+++ Talabat_Project/Program.cs
@@ -0,0 +1,39 @@
+namespace Talabat_Project
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            #region Configure Services
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddControllers();
+            #endregion
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            #region Configure Middlewares
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+            #endregion
+         
+            app.Run();
+        }
+    }
+}
